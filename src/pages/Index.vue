cc17a6f23dc1408ded87a7e48ebef59e62ba682c
Denis Khodarev <lowdog136@gmail.com>
2022-03-09 19:20:44 +0200

    Очистка и создание начальной страницы

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input
+        v-model="search"
+        placeholder="Поиск"
+        borderless
+        dark>
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      search: ''
+    }
+  }
+}
 </script>
+<style lang="scss">
+.q-page {
+background: linear-gradient(to top, #0f0c29, #302b63, #24243e )
+}
+</style>
